488e595cc83d383929bbc760c29fbcd9601cf443
allen <allen.li@tronfuture.com>
2022-12-23 18:12:40 +0800

    loguru logger

---
+++ practice/module_loguru.py
@@ -0,0 +1,9 @@
+from loguru import logger as lg
+
+# lg.trace("this is trace info")
+lg.debug("this is debug info")
+lg.info("this is info info")
+lg.success("this is success info")
+lg.warning("this is warning info")
+lg.error("this is error info")
+lg.critical("this is critical info")
