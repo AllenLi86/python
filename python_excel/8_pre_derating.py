b1986728fc0a9f4f907cd9c6e0ba75f8f591be1d
allen <allen.li@tronfuture.com>
2023-02-23 16:09:14 +0800

    20230223 16:09

--- python_excel/8_pre_derating.py
+++ python_excel/8_pre_derating.py
@@ -5,7 +5,16 @@ import re
 wb = load_workbook('example.xlsx')
 ws = wb["derating"]
 
-for i in range(4, 6):
+j=1
+while True:
+    if ws[f'G{j}'].value is None:
+        break
+    else:
+        j+=1
+   
+print("j= ", j)
+
+for i in range(4, j):
     max = str(ws[f'G{i}'].value)
     maxNum = [float(s) for s in re.findall(r'-?\d+\.?\d*', max)]
     print(maxNum)
@@ -14,14 +23,14 @@ for i in range(4, 6):
     appNum = [float(s) for s in re.findall(r'-?\d+\.?\d*', app)]
     print(appNum)
 
-    print("======")
-
     stress = f'VIN={round(appNum[0]/maxNum[0]*100)}%, IOUT={round(appNum[1]/maxNum[1]*100)}%, Tj={round(appNum[2]/maxNum[2]*100)}%'
     print(stress)
     ws[f'I{i}'] = stress
 
     state = f'{"Y" if round(appNum[0]/maxNum[0]*100) < 90 else "N"}, {"Y" if round(appNum[1]/maxNum[1]*100) < 80 else "N"}, {"Y" if appNum[2] < 110 else "N"}'
     ws[f'K{i}'] = state
+
+    print("======")
 
 wb.save("example.xlsx")
 print("--- Done! ---")
